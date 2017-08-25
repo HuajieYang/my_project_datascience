29d6ad36d31ee447190d42d224b2865762c855c9
Huajie Yang <huajie@pdx.edu>
2017-08-25 13:49:09 -0700

    A commit from my local computer

--- code/D3rd.R
+++ code/D3rd.R
@@ -6,7 +6,14 @@ testthat()
 
 # .gitignore specifiy what files should be included in repository
 
+# Check team work
 a <- 3
 b <- 4
 c <- 4
-d <- 7
+d <- 8
+
+# git codes
+git init 
+git commit
+git push
+git add
---
+++ code/D4th.R
@@ -0,0 +1,8 @@
+# Github topics 
+
+Travis CI web site test scripts. 
+
+GitHub Pages
+
+# HTTP url is 
+# Cache credentials for HTTPS
